d4232bd1e3498375a8b0c482d1096aa95d6ea7d6
Baskar Lingam Ramachandran <baskar.ramachandran@accenture.com>
2021-03-19 09:50:44 +0100

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,44 @@
-# github-actions-wait
-github-actions-wait
+# Purpose of the github action
+
+_Assumption_ : You are managing cloud infrastructure with terraform remote state in "github actions"
+
+If the same workflow is used by multiple branches or there are multiple commits in the same branch chances are that 
+the workflow runs after 1st run might get failed due to not being able to acquire lock on terraform remote state as
+1st run is already having the lock in place.
+
+To avoid this issue this github wait action can be placed in the workflow before all 'terraform apply' steps are specified
+
+By doing so the consequent runs will wait till the previous runs are complete.
+
+But when a previous completed run is rerun while there are newer runs in the queue, the newer runs will wait till the rerun is complete.
+The rerun will also wait for any run which has already crossed the wait step.  
+
+# Below are the steps to achieve parallel jobs running in github workflow to wait on a particular step
+
+1. Name the step as needed. For example: "Wait for other pipelines"
+2. Use the action "otto-de/github-actions-wait@main" with the step name as input as shown below. 
+The argument to the script should be the same as step name.
+3. Place the wait step/s in the workflow in such places so that the following steps should not run in parallel by other jobs.
+   An example of this wait step could be the terraform apply steps which use remote state lock feature. If 2 jobs try to use same remote state the 2nd job will fail. 
+4. How the script works is as below:
+   1. If the number of running jobs is 1, then there is no other job running, there will not be any waiting
+   2. If the number of running jobs is greater than 1, check the number of any jobs which were started before the current job
+      1. If there are previous jobs running, wait till all the previous jobs complete
+      2. If there are no previous jobs running, then this is a scenario needed for reruns, meaning a rerun of an older job is triggered but there are some newer jobs running as well
+         1. For each newer job running, verify if the job has already crossed wait step
+            1. If wait step is crossed for any of the newer jobs, then wait the current rerun job till the already running newer job to finish 
+            2. If no wait step is crossed for any of the newer jobs, rerun will continue execution without waiting
+           
+```
+  - name: Wait
+    env:
+      GITHUB_TOKEN: ${{ secrets.GITHUB_TOKEN }}
+    uses: otto-de/github-actions-wait@main
+    with:
+      step-name: "Wait"
+``` 
+
+# Future work
+
+1. Multiple wait steps
+2. Consider multiple jobs in the same workflow
---
+++ wait.sh
@@ -0,0 +1,122 @@
+#! /usr/bin/env bash
+
+set -exo pipefail
+
+echo "before beginning echo"
+
+step_name=$1
+
+echo "DEBUGGING: step_name is $step_name"
+
+SCRIPT_DIR="$(cd "$(dirname "$0")" ; pwd -P)"
+echo "Script dir is $SCRIPT_DIR"
+
+echo "DEBUGGING: beginning"
+
+GITHUB_WORKFLOW_URL=$(curl -sH "Authorization: Bearer ${GITHUB_TOKEN}" \
+  "https://api.github.com/repos/${GITHUB_REPOSITORY}/actions/runs/${GITHUB_RUN_ID}" | \
+  jq -r '.workflow_url')
+
+echo "run url"
+curl -sH "Authorization: Bearer ${GITHUB_TOKEN}" \
+  "https://api.github.com/repos/${GITHUB_REPOSITORY}/actions/runs/${GITHUB_RUN_ID}"
+
+echo "run jobs"
+curl -sH "Authorization: Bearer ${GITHUB_TOKEN}" \
+  "https://api.github.com/repos/${GITHUB_REPOSITORY}/actions/runs/${GITHUB_RUN_ID}/jobs"
+
+# Show the current run_number of the workflow run
+echo "CURRENT_RUN_NUMBER is ${GITHUB_RUN_NUMBER}"
+
+run_while=true
+
+while [ $run_while ];
+do
+  # If the number of runs is 1 then no further checking is needed
+  IN_PROGRESS_RUNNING=$(curl -sH "Authorization: Bearer ${GITHUB_TOKEN}" \
+    "${GITHUB_WORKFLOW_URL}/runs" | \
+    jq -r ".workflow_runs[] | select(.status == \"in_progress\")| .id" | wc -l)
+
+  echo "IN_PROGRESS_RUNNING is $IN_PROGRESS_RUNNING"
+
+  if [[ $IN_PROGRESS_RUNNING -eq 1 ]]; then
+    echo "No further checking is required. break"
+    run_while=false
+    break
+  else
+    # Get the number of workflows which are started before the current run
+    IN_PROGRESS_PREVIOUS=$(curl -sH "Authorization: Bearer ${GITHUB_TOKEN}" \
+      "${GITHUB_WORKFLOW_URL}/runs" | \
+      jq -r ".workflow_runs[] | select((.status == \"in_progress\") and (.run_number < ${GITHUB_RUN_NUMBER}))| .id" | wc -l)
+
+    echo "IN_PROGRESS_PREVIOUS is $IN_PROGRESS_PREVIOUS"
+
+    if [[ $IN_PROGRESS_PREVIOUS -gt 0 ]]; then
+      echo "Previous runs are still running. Waiting for 10 seconds"
+      sleep 10
+    else
+      echo "Validate for rerun. Better to use != than > since != would also \
+        consider any new previous run as well"
+      IN_PROGRESS_OTHER=$(curl -sH "Authorization: Bearer ${GITHUB_TOKEN}" \
+        "${GITHUB_WORKFLOW_URL}/runs" | \
+        jq -r ".workflow_runs[] | select((.status == \"in_progress\") and (.run_number != ${GITHUB_RUN_NUMBER}))| .id")
+
+      echo "IN_PROGRESS_OTHER is $IN_PROGRESS_OTHER"
+
+      IN_PROGRESS_OTHER_COUNT="${#IN_PROGRESS_OTHER[@]}"
+
+      if [[ $IN_PROGRESS_OTHER_COUNT -gt 0 ]]; then
+        echo "Wait till all other runs which have crossed wait action to complete"
+
+        for id in $IN_PROGRESS_OTHER ; do
+        # Get the run id of the run which is currently running terraform commands
+        IN_PROGRESS_ID_RUNNING=$(curl -sH "Authorization: Bearer ${GITHUB_TOKEN}" \
+          "https://api.github.com/repos/${GITHUB_REPOSITORY}/actions/runs/$id/jobs" | \
+          jq -r ".jobs[].steps[] | select((.status == \"completed\") and (.name == \"$step_name\"))| .name" | wc -l)
+
+        if [[ $IN_PROGRESS_ID_RUNNING -gt 0 ]]; then
+          echo "DEBUGGING: Have to wait till the run status of the run with id $id is completed"
+
+          echo "DEBUGGING: Get the status of the run with id $id"
+
+          run_inner_while=true
+
+          while [ $run_inner_while ]; do
+            IN_PROGRESS_ID_STATUS=$(curl -sH "Authorization: Bearer ${GITHUB_TOKEN}" \
+              "https://api.github.com/repos/${GITHUB_REPOSITORY}/actions/runs/$id" | \
+              jq -r ". | \
+              select(.status == \"completed\")| .id" | wc -l)
+
+            echo "verify if the already running github run is completed"
+
+            if [[ IN_PROGRESS_ID_STATUS -eq 1 ]]; then
+              echo "Workflow run with id $id is completed. break"
+              run_inner_while=false
+              break
+            else
+              echo "Workflow run with id $id is not yet completed. Waiting for 10 seconds"
+              sleep 10
+            fi
+          done
+        else
+          echo "For the run with id $id, the $step_name step is not completed. Continue \
+            probing other run ids which are in progress."
+          continue
+        fi
+
+      done
+      echo "There is no other run running which has crossed waitforpipeline step. break"
+      run_while=false
+      break
+
+      else
+        echo "No other run is in progress. break"
+        run_while=false
+        break
+      fi
+
+    fi
+
+  fi
+
+done
